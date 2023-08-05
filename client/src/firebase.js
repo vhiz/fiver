f99f5e0585303129c91280ba03d51ef5c29633aa
Onyedikachi Kalu <kalukachy@gmail.com>
2023-08-05 13:58:53 +0100

    message

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTU7wkju7MmvYOmf-M2MknUkOJpjWVLIQ",
-  authDomain: "fiverr-eef97.firebaseapp.com",
-  projectId: "fiverr-eef97",
-  storageBucket: "fiverr-eef97.appspot.com",
-  messagingSenderId: "822502334466",
-  appId: "1:822502334466:web:e6892959d064f17c24871b",
+  apiKey: "AIzaSyCyIoCFYAuFunLcamgSSiclvNYaRwEuqK4",
+  authDomain: "kachyco-d7f56.firebaseapp.com",
+  projectId: "kachyco-d7f56",
+  storageBucket: "kachyco-d7f56.appspot.com",
+  messagingSenderId: "566444827449",
+  appId: "1:566444827449:web:589bd54b6c8e247da73ee8"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const provider = new GoogleAuthProvider();
 export default app;
+
+
