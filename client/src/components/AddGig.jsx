766c7f76436a9cb7357d65510233360e9880ef5c
victor mgbeahurike <victormgbeahurike82@gmail.com>
2024-04-26 03:29:44 +0100

    added add new gig page and updated categories component frontend done

--- client/src/components/AddGig.jsx
+++ client/src/components/AddGig.jsx
@@ -1,5 +1,123 @@
 export default function AddGig() {
-  return <div className="modal-box">
-    
-  </div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <div className="modal-box w-[100vw] max-w-[100vw] lg:max-w-[80vw] max-h-[100vh] lg:h-[calc(100vh-5rem)]">
+      <h2 className="text-3xl font-semibold">Add Gig</h2>
+      <form method="dialog" className="lg:hidden">
+        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          ✕
+        </button>
+      </form>
+      <form
+        action=""
+        className="flex w-full gap-10 flex-col lg:flex-row"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex-1 flex flex-col gap-5">
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Title</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Category</span>
+            </div>
+            <select className="select select-bordered">
+              <option disabled selected>
+                Pick one
+              </option>
+              <option value={"design"}>Design</option>
+              <option value={"web design"}>Web Design</option>
+              <option value={"animation"}>Animation</option>
+              <option value={"music"}>Music</option>
+              <option value={"data entry"}>Data Entry</option>
+              <option value={"Seo"}>SEO</option>
+              <option value={"social"}>Social</option>
+              <option value={"ai"}>Ai Services</option>
+            </select>
+          </label>
+          <label htmlFor="" className="form-control">
+            <div className="label">
+              <span className="label-text">Images</span>
+            </div>
+            <input type="file" className="file-input file-input-ghost w-full" />
+          </label>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Description</span>
+            </div>
+            <textarea
+              className="textarea textarea-bordered h-24"
+              placeholder="Bio"
+            ></textarea>
+          </label>
+          <button className="btn btn-success w-full hidden lg:block">
+            Create
+          </button>
+        </div>
+        <div className="flex-1 flex flex-col gap-5">
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Service Title</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Delivery time(e.g 3 days)</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Revision Number</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Add Features</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Price</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <button className="btn btn-success w-full lg:hidden">Create</button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -50,6 +50,11 @@ const data = [
     title: "Translation",
     img: "/translation.jpg",
     cat: "translation",
+  }, {
+    id: 1,
+    title: "Music",
+    img: "https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    cat: "Music",
   },
 ];
 export default function Categories() {
